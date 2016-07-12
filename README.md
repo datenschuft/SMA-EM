8f7fc71ad81175568f176b5fa33f1cab56c3e0a6
Wenger Florian <wenger@unifox.at>
2016-07-12 22:19:38 +0200

    configure daemon

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 SMA Energymeter measurement
 sma-em-measurement.py: Python3 loop dieplay SMA Energymeter measurement values
 
-sma-daemon.py: Python3 daemon writing regard and surplus values to /run/shm/em-regard and /run/shm/em-surplus
+sma-daemon.py: Python3 daemon writing regard and surplus values to /run/shm/em-[serial]-[value]
 
+create a config file in /etc/smaemd/config
+Example content
+
+
+[SMA-EM]
+# serials of sma-ems the daemon should take notice
+# seperated by space
+serials=123456 1900204522
+# measurement values
+values=pregard psurplus qsurplus ssurplus
+
+[DAEMON]
+pidfile=/run/smaemd.pid
 
--- sma-daemon.py
+++ sma-daemon.py
@@ -1,46 +1,55 @@
 #!/usr/bin/python3
 # coding=utf-8
 """
- * 
- * by Wenger Florian 2015-11-12
+ *
+ * by Wenger Florian 2016-07-12
  * wenger@unifox.at
- * 
+ *
  *  this software is released under GNU General Public License, version 2.
- *  This program is free software; 
- *  you can redistribute it and/or modify it under the terms of the GNU General Public License 
+ *  This program is free software;
+ *  you can redistribute it and/or modify it under the terms of the GNU General Public License
  *  as published by the Free Software Foundation; version 2 of the License.
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU General Public License along with this program; 
- *  if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  
- * 
+ *
+ *  You should have received a copy of the GNU General Public License along with this program;
+ *  if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  */
 """
-  
 import sys, time
 from daemon3x import daemon3x
+from configparser import SafeConfigParser
 import smaem
-  
+
+#read configuration
+parser = SafeConfigParser()
+parser.read('/etc/smaemd/config')
+
+smaemserials=parser.get('SMA-EM', 'serials')
+serials=smaemserials.split(' ')
+smavalues=parser.get('SMA-EM', 'values')
+values=smavalues.split(' ')
+pidfile=parser.get('DAEMON', 'pidfile')
+
 class MyDaemon(daemon3x):
 	def run(self):
 		emparts = {}
 		while True:
-			#time.sleep(1)
-			#print ("1")
-			#readem()
 			emparts=smaem.readem()
-			file = open("/run/shm/em-regard", "w")
-			file.write('%.2f' % emparts['pregard'])
-			file.close()
-			file = open("/run/shm/em-surplus", "w")
-			file.write('%.2f' % emparts['psurplus'])
-			file.close() 
+			for serial in serials:
+				#print(serial)
+				#print(emparts['serial'])
+				if serial==format(emparts['serial']):
+					#print("match")
+					for value in values:
+						file = open("/run/shm/em-"+format(serial)+"-"+format(value), "w")
+						file.write('%.4f' % emparts[value])
+						file.close()
 
-  
 if __name__ == "__main__":
-	daemon = MyDaemon('/tmp/daemon-example.pid')
+	daemon = MyDaemon(pidfile)
 	if len(sys.argv) == 2:
 		if 'start' == sys.argv[1]:
 			daemon.start()
@@ -54,4 +63,5 @@ if __name__ == "__main__":
 		sys.exit(0)
 	else:
 		print ("usage: %s start|stop|restart" % sys.argv[0])
+		print (pidfile)
 		sys.exit(2)
